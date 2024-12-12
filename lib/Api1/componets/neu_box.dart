6b50a20c8497033acdbc5f957ca182a01b9538a0
rinkalbaldaniya <155662107+rinkalbaldaniya186@users.noreply.github.com>
2024-12-12 09:20:00 +0530

    Update neu_box.dart

--- lib/Api1/componets/neu_box.dart
+++ lib/Api1/componets/neu_box.dart
@@ -6,6 +6,7 @@ class NeuBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool isDarkMode = Provider.of<ThemeProvider>(context).isDarkMode;
     return Container(
       decoration: BoxDecoration(
         color: Theme.of(context).colorScheme.background,
@@ -13,13 +14,13 @@ class NeuBox extends StatelessWidget {
           boxShadow: [
             // darker shadow on bottom right
            BoxShadow(
-             color: Colors.grey.shade500,
+             color: isDarkMode ? Colors.black : Colors.grey.shade500,
              blurRadius: 15,
              offset: Offset(4, 4),
            ),
           // lighter shadow on top left
           BoxShadow(
-            color: Colors.white,
+            color:  isDarkMode ? Colors.grey.shade800 : Colors.white,
             blurRadius: 15,
             offset: Offset(-4, -4),
           ),
